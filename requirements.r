021fe75a1e037ea3696ba4deba997109a764839b
Filip Schouwenaars <filip@datacamp.com>
2017-02-15 12:54:42 +0100

    specify requirements

---
+++ requirements.r
@@ -0,0 +1,6 @@
+devtools::install_version("dplyr", "0.5.0")
+devtools::install_version("tidyr", "0.6.1")
+devtools::install_version("ggplot2", "2.2.1")
+source("https://bioconductor.org/biocLite.R")
+biocLite("GEOquery")
+biocLite("Biobase")
